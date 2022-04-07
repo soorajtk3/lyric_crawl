3c337ccf24f96b66c31f5c3611e1502298a7d83c
sooraj <sooraj@hamon.in>
2022-04-07 21:20:00 +0530

    web application using jquery done

--- appflask.py
+++ appflask.py
@@ -6,32 +6,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    #artists = get_data.get_all_artist()
-    return render_template("base.html")
+    return render_template("base.html",)
 
 
-@app.route("/artist")
-def display_artist():
+@app.route("/artists")
+def get_artists():
     artists = get_data.get_all_artist()
-    artist_arr = [{'id': i[0], 'name':i[1]}for i in artists]
-    return jsonify(artist_arr)
+    artists_arr = [{'id': i[0], "name":i[1]} for i in artists]
+    return jsonify(artists_arr)
 
 
 @app.route("/songs/<int:aid>")
 def list_all_songs(aid):
     songs = get_data.get_all_songs(aid)
-    artists = get_data.get_all_artist()
-    artist = get_data.singer(aid)
-    return render_template("songlist.html", artist=artist, artists=artists, songs=songs, current=aid)
+    songs_arr = [{'id': i[1], "name":i[0]} for i in songs]
+    # singer= get_data.singer(aid)
+    # artists = get_data.get_all_artist()
+    return jsonify(songs_arr)
 
 
 @app.route("/songs/<int:aid>/lyrics/<int:sid>")
 def lyrics(sid, aid):
     lyrics = get_data.get_lyrics(sid)
     songs = get_data.get_all_songs(aid)
+    singer = get_data.singer(aid)
     artists = get_data.get_all_artist()
-    artist = get_data.singer(aid)
-    return render_template("lyrics.html", lyrics=lyrics, artist=artist, artists=artists, songs=songs, current=aid, csong=sid)
+    print(lyrics)
+    return jsonify(lyrics)
 
 
 if __name__ == "__main__":
--- static/site.js
+++ static/site.js
@@ -1,15 +1,39 @@
 function main() {
   $.get({
-    url: 'http://127.0.0.1:5000/artist',
+    url: 'http://127.0.0.1:5000/artists',
     success: (data) => {
       list = '';
       data.forEach((element) => {
-        list += '<li class="list-box">' + element.name + '</li>';
+        list +=
+          `<li class="artistbox" value=${element.id}>` + element.name + `</li>`;
       });
-      tag = `<ul>${list}<ul/>`;
-      $('div.artist').html(tag);
-      console.log(data);
+      tag = `<h4>Artists</h4><ul type="none">${list}</ul>`;
+      $('div.artist_list').html(tag);
+      // console.log(data);
     },
+  });
+  $(document).on('click', 'li.artistbox', function () {
+    $.get({
+      url: `http://127.0.0.1:5000/songs/${this.value}`,
+      success: (data) => {
+        list = '';
+        data.forEach((element) => {
+          list +=
+            `<li class="songbox" id=${element.id}>` + element.name + `</li>`;
+        });
+        tag = `<h4>Songs</h4><ul type="none">${list}</ul>`;
+        $('div.song_list').html(tag);
+      },
+    });
+  });
+  $(document).on('click', 'li.songbox', function () {
+    $.get({
+      url: `http://127.0.0.1:5000/songs/${this.value}/lyrics/${this.id}`,
+      success: (data) => {
+        lyrics = `<h4 class="lhead">Lyrics</h4><pre><p>${data}</p></pre>`;
+        $('div.lyrics').html(lyrics);
+      },
+    });
   });
 }
 $(main);
