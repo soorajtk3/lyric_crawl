46b80f8c48680d9c69bd54d5305eea66f2e4c1e7
sooraj <sooraj@hamon.in>
2022-04-11 15:58:03 +0530

    API written

--- api.py
+++ api.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, jsonify
+import get_data
 
 api = Blueprint('api', __name__)
 
 
-@api.route("/artist")
+@api.route("/artists")
 def get_artists():
-    return jsonify({"name": "sooraj"})
+    artists = get_data.get_all_artist()
+    artists_arr = [{'id': i[0], "name":i[1]} for i in artists]
+    return jsonify(artists_arr)
+
+
+@api.route("/songs/<int:aid>")
+def get_songs(aid):
+    songs = get_data.get_all_songs(aid)
+    songs_arr = [{'id': i[1], "name":i[0]} for i in songs]
+    return jsonify(songs_arr)
+
+
+@api.route("/songs/<int:aid>/lyrics/<int:sid>")
+def lyrics(sid, aid):
+    lyrics = get_data.get_lyrics(sid)
+    songs = get_data.get_all_songs(aid)
+    return jsonify(lyrics)
--- appflask.py
+++ appflask.py
@@ -1,10 +1,8 @@
+import api
 from distutils.log import debug
 from flask import Flask, render_template, jsonify
-import get_data
-import api
-
-
 app = Flask(__name__)
+
 
 app.register_blueprint(api.api, url_prefix="/api")
 
@@ -14,31 +12,51 @@ def hello():
     return render_template("base.html",)
 
 
-@app.route("/artists")
-def get_artists():
-    artists = get_data.get_all_artist()
-    artists_arr = [{'id': i[0], "name":i[1]} for i in artists]
-    return jsonify(artists_arr)
-
-
-@app.route("/songs/<int:aid>")
-def list_all_songs(aid):
-    songs = get_data.get_all_songs(aid)
-    songs_arr = [{'id': i[1], "name":i[0]} for i in songs]
-    # singer= get_data.singer(aid)
-    # artists = get_data.get_all_artist()
-    return jsonify(songs_arr)
-
-
-@app.route("/songs/<int:aid>/lyrics/<int:sid>")
-def lyrics(sid, aid):
-    lyrics = get_data.get_lyrics(sid)
-    songs = get_data.get_all_songs(aid)
-    singer = get_data.singer(aid)
-    artists = get_data.get_all_artist()
-    # print(lyrics)
-    return jsonify(lyrics)
-
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# from distutils.log import debug
+# from flask import Flask, render_template, jsonify
+# import get_data
+# import api
+
+
+# app = Flask(__name__)
+
+# app.register_blueprint(api.api, url_prefix="/api")
+
+
+# @app.route("/")
+# def hello():
+#     return render_template("base.html",)
+
+
+# @app.route("/artists")
+# def get_artists():
+#     artists = get_data.get_all_artist()
+#     artists_arr = [{'id': i[0], "name":i[1]} for i in artists]
+#     return jsonify(artists_arr)
+
+
+# @app.route("/songs/<int:aid>")
+# def list_all_songs(aid):
+#     songs = get_data.get_all_songs(aid)
+#     songs_arr = [{'id': i[1], "name":i[0]} for i in songs]
+#     # singer= get_data.singer(aid)
+#     # artists = get_data.get_all_artist()
+#     return jsonify(songs_arr)
+
+
+# @app.route("/songs/<int:aid>/lyrics/<int:sid>")
+# def lyrics(sid, aid):
+#     lyrics = get_data.get_lyrics(sid)
+#     songs = get_data.get_all_songs(aid)
+#     singer = get_data.singer(aid)
+#     artists = get_data.get_all_artist()
+#     # print(lyrics)
+#     return jsonify(lyrics)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
