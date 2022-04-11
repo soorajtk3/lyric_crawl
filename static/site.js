048c6e8b860a282aeffbc9dcaa0bf1c873d928a7
sooraj <sooraj@hamon.in>
2022-04-11 16:00:06 +0530

    API written in separate file and song lyrics showing on website

--- static/site.js
+++ static/site.js
@@ -1,6 +1,6 @@
 function main() {
   $.get({
-    url: 'http://127.0.0.1:5000/artists',
+    url: 'http://127.0.0.1:5000/api/artists',
     success: (data) => {
       list = '';
       data.forEach((element) => {
@@ -9,12 +9,22 @@ function main() {
       });
       tag = `<h4>Artists</h4><ul type="none">${list}</ul>`;
       $('div.artist_list').html(tag);
-      // console.log(data);
     },
   });
+
+  $(document).on('click', 'li.artistbox', function () {
+    $('li.artistbox.current').removeClass('current');
+    $(this).addClass('current');
+  });
+
+  $(document).on('click', 'li.songbox', function () {
+    $('li.songbox.current').removeClass('current');
+    $(this).addClass('current');
+  });
+
   $(document).on('click', 'li.artistbox', function () {
     $.get({
-      url: `http://127.0.0.1:5000/songs/${this.value}`,
+      url: `http://127.0.0.1:5000/api/songs/${this.value}`,
       success: (data) => {
         list = '';
         data.forEach((element) => {
@@ -26,9 +36,10 @@ function main() {
       },
     });
   });
+
   $(document).on('click', 'li.songbox', function () {
     $.get({
-      url: `http://127.0.0.1:5000/songs/${this.value}/lyrics/${this.id}`,
+      url: `http://127.0.0.1:5000/api/songs/${this.value}/lyrics/${this.id}`,
       success: (data) => {
         lyrics = `<h4 class="lhead">Lyrics</h4><pre><p>${data}</p></pre>`;
         $('div.lyrics').html(lyrics);
